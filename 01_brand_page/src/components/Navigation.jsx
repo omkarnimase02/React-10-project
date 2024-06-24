b7330c2daf266233e9f6f2984d7f68a4e18d33a9
Omkar Nimase <omkar.nimase20@vit.edu>
2024-06-24 14:17:04 +0530

    Brand page Done

---
+++ 01_brand_page/src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+const Navigation=()=> {
+  return (
+    <nav>
+
+        <div className="logo">
+        <img src="nike.png" alt="logo" />
+        </div>
+
+        <ul>
+        <li herf="#">Menu</li>
+        <li herf="#">Location </li>
+        <li herf="#">About</li>
+        <li herf="#">Contact</li>
+        </ul>
+
+        <button className='but'>Login</button>
+        
+
+      </nav> 
+  )   
+      
+}
+
+
+export default Navigation;
