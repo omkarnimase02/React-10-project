8251989e0cba8cdb3a793b1772e1abee02b01fc4
Omkar Nimase <omkar.nimase20@vit.edu>
2024-06-25 08:41:09 +0530

    project_2 Done

---
+++ 02_contact_us/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,80 @@
+import Button from "../Button/Button";
+import styles from "./ContactForm.module.css";
+import { MdMessage } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
+import { useState } from "react";
+
+
+const ContactForm = () => {
+
+  //useState
+  const [name,setName]  = useState("Omkar");
+  const [email,setEmail]  = useState("omnimase08@gmail.com");
+  const [text,setText]  = useState("Currently study in VIT college");
+ 
+
+  // function
+  const onSubmit=(e)=>{
+
+    
+    e.preventDefault();
+
+    setName(e.target[0].value);
+    setEmail(e.target[1].value);
+    setText(e.target[2].value);
+  };
+
+  
+
+  
+  return (
+    <section className={styles.container}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+        </div>
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<HiMail fontSize="24px" />}
+        />
+
+        <form
+         onSubmit={onSubmit}>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="text">Text</label>
+            <textarea name="text" rows="8" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <Button text="SUBMIT BUTTON" />
+          </div>
+
+          <div>{`My Name is ${name}, mail is ${email} and i am adding some text about me ${text}`}</div>
+        </form>
+      </div>
+      <div className={styles.contact_image}>
+        <img src="/images/contact.svg" alt="contact image" />
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
