11ab0d130b3b6de8c333b77ebcc4070ec00808d8
Omkar Nimase <omkar.nimase20@vit.edu>
2024-06-26 12:43:34 +0530

    Dice Game Complete

---
+++ 03_Dice_game/src/components/RoleDice.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+const RoleDice = ({currentDice,roleDice}) => {
+
+    
+    
+
+  return (
+    
+    <DiceContainer>
+        <div className='dice'
+        onClick={roleDice }>
+            <img src= {`/images/dice/dice_${currentDice}.png`} alt="dice_1" />
+        </div>
+        <p>Click on Dice to roll</p>
+    </DiceContainer>
+  )
+}
+
+export default RoleDice;
+
+const DiceContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 48px;
+
+.dice{
+    cursor: pointer;
+}
+
+p{
+    font-size: 24px;
+
+
+}
+
+    
+`;
